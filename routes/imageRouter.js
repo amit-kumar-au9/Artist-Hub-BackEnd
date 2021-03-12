68b0974bc6a3b335d4658143c20e5bc822de791e
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-12 18:30:42 +0530

    crud on images completed

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -1,10 +1,19 @@
 const Router = require('express').Router();
 
-const { addImage } = require('../controllers/imageVideoController');
+const {
+	addImage,
+	getPostImages,
+	deleteImage,
+	updateImage,
+} = require('../controllers/imageVideoController');
+
 const authChecker = require('../auth/isAuth');
 
 const { ImageValidation } = require('../validations/imageValidation');
 
 Router.post('/addImage', authChecker, ImageValidation, addImage);
+Router.post('/updateImages', authChecker, updateImage);
+Router.post('/deleteImages', authChecker, deleteImage);
+Router.get('/getImages/:postId', authChecker, getPostImages);
 
 module.exports = Router;
