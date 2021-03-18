3334b57f95656fcd2f55d1a4e0f108ad0a97d1cb
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-18 15:26:17 +0530

    added explore page file

---
+++ models/explorePageModel.js
@@ -0,0 +1,74 @@
+const postSchema = require('../schema/postSchema');
+
+exports.getAllPost = (callback) => {
+	try {
+		postSchema
+			.aggregate([
+				{ $match: { active: 1.0 } },
+				{ $addFields: { userId: { $toObjectId: '$userId' } } },
+				{
+					$lookup: {
+						from: 'users',
+						localField: 'userId',
+						foreignField: '_id',
+						as: 'userData',
+					},
+				},
+				{
+					$project: {
+						'userData.type': 0.0,
+						'userData.occassions': 0,
+						'userData.email': 0,
+						'userData.password': 0,
+						'userData.coverPic': 0,
+						'userData.shortDesc': 0,
+						'userData.date': 0,
+						'userData.__v': 0,
+						'userData.coverPicId': 0,
+						'userData.profilePicId': 0,
+					},
+				},
+				{ $match: { 'userData.isActive': 1 } },
+				{ $sort: { date: -1.0 } },
+			])
+			.then((reply) => {
+				if (reply.length) {
+					callback('', {
+						message: 'All post send',
+						status: 200,
+						data: reply,
+					});
+				} else {
+					callback('', {
+						message: 'No post found',
+						status: 400,
+					});
+				}
+			})
+			.catch((err) => callback(err));
+	} catch (error) {
+		callback(error);
+	}
+};
+
+exports.getPostByOccasssion = (userId, callback) => {
+	try {
+		callback('', {
+			message: 'Logic not ready',
+			status: 400,
+		});
+	} catch (error) {
+		callback(error);
+	}
+};
+
+exports.getPostByTag = (userId, callback) => {
+	try {
+		callback('', {
+			message: 'Logic not ready',
+			status: 400,
+		});
+	} catch (error) {
+		callback(error);
+	}
+};
--- routes/homePageRouter.js
+++ routes/homePageRouter.js
@@ -3,7 +3,6 @@ const Router = require('express').Router();
 const {
 	getAllPost,
 	getPostForYou,
-	getLatestPost,
 	getMostRatedPost,
 	getTrendingPost,
 	getPostByOccasssion,
@@ -11,11 +10,14 @@ const {
 } = require('../controllers/homePageController');
 
 Router.get('/getAllPost', getAllPost); //sort by time
-Router.get('/getPostForYou', getPostForYou); //post of followed artist
-Router.get('/getLatestPost', getLatestPost); //sort by time of followed artist
+Router.get('/getPostForYou', getPostForYou); //post of followed artist by latest post
+
 Router.get('/getMostRatedPost', getMostRatedPost); //sort by rating
-Router.get('/getTrendingPost', getTrendingPost); //sort by likes and rating
+Router.get('/getTrendingPost', getTrendingPost); //sort by likes
+
 Router.get('/getPostByOccasssion/:type', getPostByOccasssion); //search post by occassion
 Router.get('/getPostByTag/:tag', getPostByTag); //search post by tags
 
 module.exports = Router;
+
+// Router.get('/getLatestPost', getLatestPost); //sort by time of followed artist
