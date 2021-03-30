a902dacbc81f07844f06c91978046ed048ab31c4
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-31 01:32:10 +0530

    forgot password

--- src/api/routes/authRouter.js
+++ src/api/routes/authRouter.js
@@ -5,16 +5,22 @@ const {
 	registerUser,
 	logoutUser,
 	getUserDetail,
+	emailExist,
+	passwordReset,
 } = require('../controllers/authController');
 
 const {
 	loginValidation,
 	registerValidation,
+	resetPassword,
 } = require('../validations/authValidation');
 
 Router.post('/registerUser', registerValidation, registerUser);
 Router.post('/loginUser', loginValidation, loginUser);
 Router.get('/logout', logoutUser);
 Router.get('/getUserDetail', authChecker, getUserDetail);
+// forgot password
+Router.get('/emailExist', emailExist);
+Router.post('/passwordReset', resetPassword, passwordReset);
 
 module.exports = Router;
